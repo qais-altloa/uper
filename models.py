8ad4b54cf6daa7831dd99ca8add14185f5118a30
23-101344 <23-101344@students.eui.edu.eg>
2026-05-07 21:58:32 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,6 @@
 # models.py
 # Data classes for Driver and RideRequest
 
-class Driver:
-   
-
-class RideRequest:
-   
-
 class Cell:
     def __init__(self, row, col):
       self.row=row
@@ -16,8 +10,9 @@ class Cell:
       
     def __repr__(self):
         return f"Cell({self.row}, {self.col})"
+
 class Driver:
-  def __init__(self, id, name, phone, car, row, col):
+   def __init__(self, id, name, phone, car, row, col):
         self.id = id       
         self.name = name    
         self.phone = phone 
@@ -25,10 +20,13 @@ class Driver:
         self.position = Cell(row, col)
         self.available = True 
   def __repr__(self):
-        return f"Driver({self.id}, {self.name},{self.phone},{self.car},{self.position},{self.available})"       
-class Passenger:
-  def __init__(self, pickup_row, pickup_col, dest_row, dest_col):
+        return f"Driver({self.id}, {self.name},{self.phone},{self.car},{self.position},{self.available})" 
+class RideRequest:
+   def __init__(self, pickup_row, pickup_col, dest_row, dest_col):
     self.pickup = Cell(pickup_row, pickup_col)
     self.destination = Cell(dest_row, dest_col)
   def __repr__(self):
-        return f"Passenger({self.pickup},{self.destination})"
+        return f"RideRequest({self.pickup},{self.destination})"
+   
+
+
