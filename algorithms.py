43ae593a2170762b7d963691dbcf41f22ceb9beb
23-101060 <23-101060@students.eui.edu.eg>
2026-05-08 02:30:08 +0300

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -4,29 +4,23 @@ class CostMatrix:
     def __init__(self, drivers, requests):
         self.drivers = drivers
         self.requests = requests
-        # create empty matrix
         self.matrix = []
 
     # ─────────────────────────────────────────
 
     def compute(self):
-        # loop on every driver
         for driver in self.drivers:
             row = []
-            # loop on every request
             for request in self.requests:
                 pickup = request.passenger.pickup
-                # Manhattan Distance
                 cost = abs(driver.position.row - pickup.row) + \
                        abs(driver.position.col - pickup.col)
                 row.append(cost)
             self.matrix.append(row)
 
     # ─────────────────────────────────────────
-
     def get_cost(self, driver_index, request_index):
         return self.matrix[driver_index][request_index]
-
     # ─────────────────────────────────────────
     def display(self):
         print("Cost Matrix:\n")
