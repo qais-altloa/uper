7c86a5e4155eca763ab67baebe5535a5b81eef80
Moataz Wael Saber <24-101094@students.eui.edu.eg>
2026-05-07 20:54:35 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,11 @@
-class Cell:
-    def __init__(self, row, col):
-      self.row=row
-      self.col = col
-    def __eq__(self, other):
-      return self.row == other.row and self.col == other.col 
-      
-    def __repr__(self):
-        return f"Cell({self.row}, {self.col})"
+# models.py
+# Data classes for Driver and RideRequest
+
+class Driver:
+   
+
+class RideRequest:
+   
+
+    def __repr__():
+       
