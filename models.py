053c72fa025f928dbe7549ae67235af29ab548f7
23-101344 <23-101344@students.eui.edu.eg>
2026-05-07 21:03:39 +0300

    Update models.py

--- models.py
+++ models.py
@@ -16,4 +16,19 @@ class Cell:
       
     def __repr__(self):
         return f"Cell({self.row}, {self.col})"
-       
+class Driver:
+  def __init__(self, id, name, phone, car, row, col):
+        self.id = id       
+        self.name = name    
+        self.phone = phone 
+        self.car = car
+        self.position = Cell(row, col)
+        self.available = True 
+  def __repr__(self):
+        return f"Driver({self.id}, {self.name},{self.phone},{self.car},{self.position},{self.available})"       
+class Passenger:
+  def __init__(self, pickup_row, pickup_col, dest_row, dest_col):
+    self.pickup = Cell(pickup_row, pickup_col)
+    self.destination = Cell(dest_row, dest_col)
+  def __repr__(self):
+        return f"Passenger({self.pickup},{self.destination})"
