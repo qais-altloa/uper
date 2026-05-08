5f4f059ce1ffc896d108a0dbc6c55df7d15d47aa
23-101344 <23-101344@students.eui.edu.eg>
2026-05-08 19:46:17 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,15 +1,16 @@
 class Cell:
+    # Represents a single position (row, col) in the grid
     def __init__(self, row, col):
         self.row = row
         self.col = col
-
+    # Compare two cells based on their coordinates
     def __eq__(self, other):
         return self.row == other.row and self.col == other.col
-
     def __repr__(self):
         return f"Cell({self.row}, {self.col})"
 
 class Driver:
+    # Represents a driver in the system
     def __init__(self, id, name, phone, car, row, col):
         self.id = id
         self.name = name
@@ -24,6 +25,7 @@ class Driver:
                 f"position={self.position}, available={self.available})")
 
 class Passenger:
+    # Represents a passenger request with pickup and destination
     def __init__(self, pickup_row, pickup_col, dest_row, dest_col):
         self.pickup = Cell(pickup_row, pickup_col)
         self.destination = Cell(dest_row, dest_col)
@@ -33,9 +35,10 @@ class Passenger:
 
 
 class RideRequest:
+    # Represents a ride request made by a passenger
     def __init__(self, id, passenger):
-        self.id = id
-        self.passenger = passenger
+        self.id = id    # Unique request ID
+        self.passenger = passenger    # store the passenger inside the request
 
     def __repr__(self):
         return f"RideRequest(id={self.id}, passenger={self.passenger})"
