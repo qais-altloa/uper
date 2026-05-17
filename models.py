216243fa27ccc8fc98ba4fa341d3478b1a43f6dd
Moataz Wael Saber <24-101094@students.eui.edu.eg>
2026-05-18 00:17:49 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,52 +1,46 @@
 # models.py
-# Core data models for the Uber system
 
 class Cell:
-    """Represents a single position (row, col) in the grid"""
     def __init__(self, row, col):
         self.row = row
         self.col = col
-    
+
+    def manhattan(self, other):
+        return abs(self.row - other.row) + abs(self.col - other.col)
+
     def __eq__(self, other):
         return self.row == other.row and self.col == other.col
-    
+
+    def __hash__(self):
+        return hash((self.row, self.col))
+
     def __repr__(self):
         return f"({self.row},{self.col})"
-    
-    def manhattan_distance(self, other):
-        return abs(self.row - other.row) + abs(self.col - other.col)
 
 
 class Driver:
-    """Represents a driver in the system"""
-    def __init__(self, id, name, phone, car, row, col):
-        self.id = id
+    def __init__(self, driver_id, name, row, col):
+        self.id = driver_id
         self.name = name
-        self.phone = phone
-        self.car = car
         self.position = Cell(row, col)
         self.available = True
-        self.current_path = []
-    
+        self.rating = round(__import__('random').uniform(3.0, 5.0), 1)
+        self.trips = 0
+
+    def add_rating(self, stars):
+        """Update rating as a running average (stars: 1-5)."""
+        self.rating = round((self.rating * self.trips + stars) / (self.trips + 1), 1)
+        self.trips += 1
+
     def __repr__(self):
-        return f"Driver(id={self.id}, name={self.name}, position={self.position})"
+        return f"Driver(D{self.id}, {self.name}, rating={self.rating}, pos={self.position})"
 
 
-class Passenger:
-    """Represents a passenger request with pickup and destination"""
-    def __init__(self, pickup_row, pickup_col, dest_row, dest_col):
+class Request:
+    def __init__(self, request_id, pickup_row, pickup_col, dest_row, dest_col):
+        self.id = request_id
         self.pickup = Cell(pickup_row, pickup_col)
         self.destination = Cell(dest_row, dest_col)
-    
-    def __repr__(self):
-        return f"Passenger(pickup={self.pickup}, dest={self.destination})"
 
-
-class RideRequest:
-    """Represents a ride request made by a passenger"""
-    def __init__(self, id, passenger):
-        self.id = id
-        self.passenger = passenger
-    
     def __repr__(self):
-        return f"RideRequest(id={self.id}, passenger={self.passenger})"
+        return f"Request(R{self.id}, pickup={self.pickup}, dest={self.destination})"
